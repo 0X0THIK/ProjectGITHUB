c2f1e3b7d5b3d8260ae74e1951783d507c7feb51
0X0THIK <sashapetkevich26@gmail.com>
2022-04-01 17:49:52 +0300

    Delete all trash

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,6 @@
 using namespace std;
 
 int main() {
-	int a, b;
-
-	cout << "Input your number: " << endl;
-	cin >> a >> b;
-
-	int max = 0;
-
-	if (a > b)
-	{
-		max = a;
-	}
-	else
-	{
-		max = b;
-	}
-
-	cout << "Max = " << max << endl;
-
+	
 	return 0;
 }
